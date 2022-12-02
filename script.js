df5e0c495385a9ef0aa85efb64043499fc890b3a
Dipto Das <ddipto396@gmail.com>
2022-12-01 18:54:33 -0800

    minor correction

--- script.js
+++ script.js
@@ -3,17 +3,22 @@ let icon2 = document.querySelector('.icon2');
 let menu = document.querySelector('.menu');
 let rightSide_menubar = document.querySelector('.rightSide-menubar');
 let list_menubar = document.querySelector('.list-menubar');
+let inner_container = document.querySelector('.inner-container');
 
 
 icon1.addEventListener('click', ()=>{
     list_menubar.innerHTML = rightSide_menubar.innerHTML;
     menu.style.visibility = 'visible';
     menu.style.opacity = '1';
+    menu.style.zIndex = 1;
+    inner_container.style.zIndex = 0;
 })
 
 icon2.addEventListener('click', ()=>{
     menu.style.visibility = 'hidden';
     menu.style.opacity = '';
+    menu.style.zIndex = 0;
+    inner_container.style.zIndex = 1;
 })
 
 let x = window.matchMedia("(min-width:769px)")
